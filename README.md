e6543f7c9909b90ca44e54e7c436014a826e7e1a
Leonid Keselman <lkeselman@gmail.com>
2022-07-19 18:32:57 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# fuzzy-metaballs
+# Fuzzy Metaballs Differentiable Renderer
+Here is a simplified version of our code base, without all the specific code to generate our plots, visualizations and run experiments on our custom dataset. Likewise we only feature the linear intersection rule, as it is the main method used in our paper. 
+
+# Shape From Silhouette
+This sample builds on the PyTorch3D model reconstruction tutorial, except the method uses only silhouettes and uses Fuzzy Metaballs. Should run in about 1 minute on a laptop.
+
+# Pose Estimation
+This sample builds on the PyTorch3D camera pose optimization tutorial, except the method uses depth and silhouette along with pose perturbation. 
---
+++ fm_render.py
@@ -0,0 +1,59 @@
+import jax
+import jax.numpy as jnp
+
+def jax_stable_exp(z,s=1,axis=0):
+    z = s*z
+    z = z- z.max(axis)
+    z = jnp.exp(z)
+    return z
+
+def render_func(means, prec_full, weights_log, camera_rays, axangl, t, beta_1, beta_2, beta_3):
+    prec = jnp.triu(prec_full)
+    weights = jnp.exp(weights_log)
+    weights = weights/weights.sum()
+
+    scale = jnp.sqrt(axangl @ axangl)
+    vec = axangl/scale
+    Rx = jnp.array([[0,0,0],[0,0,-1.0],[0,1,0]])
+    Ry = jnp.array([[0,0,1],[0,0,0],[-1.0,0,0]])
+    Rz = jnp.array([[0,-1.0,0],[1,0,0],[0,0,0]])
+    Rmat = jnp.array([Rx,Ry,Rz])
+    K = Rmat @ vec
+    ctheta = jnp.cos(scale)
+    stheta = jnp.sin(scale)
+    Rest = jnp.where(scale > 1e-12,(jnp.eye(3) + stheta*K + (1-ctheta)*(K@K)), jnp.eye(3)).T
+    camera_rays = camera_rays @ Rest
+    zs = []
+    probs = []
+    def perf_idx(i):
+        prc = prec[i].T
+        #prc = jnp.diag(jnp.sign(jnp.diag(prc))) @ prc
+        div = jnp.prod(jnp.diag(jnp.abs(prc))) + 1e-20
+
+        w = weights[i] 
+        p =  means[i] -t 
+        def perf_ray(r):
+            vsv = ((prc @ r)**2).sum()
+            psv = ((prc @ p) * (prc@r)).sum()
+
+            # linear
+            res = (psv)/(vsv)
+            
+            v = r * res - p
+
+            d0 = ((prc @ v)**2).sum()
+            d2 = -0.5*d0 + jnp.log(w)
+            d3 =  d2 + jnp.log(div)
+
+            return res,d2, d3
+        res,d2,d3  = jax.vmap((perf_ray))(camera_rays) # jit perf
+        return res, d2, d3
+
+    zs,stds,probs = jax.vmap(perf_idx)(jnp.arange(means.shape[0]))  # jit perf
+    sig1 = jnp.tanh(beta_1 * zs)*0.5 + 0.5 # sigmoid
+
+    w = jnp.nan_to_num(jax_stable_exp(-zs*beta_2 + sig1*beta_3*probs))+1e-20
+
+    wgt  = w.sum(0)
+    init_t=  (w*jnp.nan_to_num(zs)).sum(0)/jnp.where(wgt==0,1,wgt)
+    return init_t,stds
